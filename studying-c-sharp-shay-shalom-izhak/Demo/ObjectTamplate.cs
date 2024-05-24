3f99f882bc8efde17ba0c9d4742f97a59263023a
shay shalom izhak <alonizhakalonizhak@gmail.com>
2024-05-24 12:08:33 +0300

    my new changes

--- studying-c-sharp-shay-shalom-izhak/Demo/ObjectTamplate.cs
+++ studying-c-sharp-shay-shalom-izhak/Demo/ObjectTamplate.cs
@@ -32,6 +32,52 @@ namespace studying_c_sharp_shay_shalom_izhak.Demo
 
 
     }
+    public class Chair 
+    {
+        public Chair() { }
+        public Chair(string color, int height , bool isWood)
+        {
+           this.color = color;
+           this.height = height;
+           this.isWood = isWood;
+        }
+
+        public string color = "white";
+        public int height = 46;
+        public bool isWood = false;
+        public void DisplayProps()
+        {
+            Console.WriteLine("chair = [color = {0}, height = {1}, isWood = {2}]", color, height, isWood);
+        }
+
+    }
+    public class Room 
+    {
+        public string type = "bathroom";
+        public int sqm = 10;
+        public bool hasWindows = true;
+        public Chair chair = new Chair();
+
+        public void DisplayProps()
+        {
+            Console.WriteLine("room = [type = {0}] ",type );
+            chair.DisplayProps();
+        }
+    }
+    public class Home 
+    {
+        public Room[] rooms = new Room[3];
+        public Home()
+        {
+            rooms[0] = new Room();
+        }
+        public Home(string type)
+        {
+            rooms[0] = new Room();
+            rooms[0].type = type;
+        }
+    }
+
 
     public class TestBasicObject
     {
@@ -64,7 +110,19 @@ namespace studying_c_sharp_shay_shalom_izhak.Demo
             Console.WriteLine("Template 2 = intProperty = {0} , string property = {1}, boolProperty ={2} "
                , template2.intProperty, template2.stringProperty, template2.boolProperty);
 
+            Chair c1 = new Chair();
+            c1.DisplayProps();
+            c1.color = "Black";
+            c1.DisplayProps();
 
+            Home home = new Home();
+            home.rooms[0].DisplayProps();
+
+            Home home2 = new Home("warhouse");
+            home2.rooms[0].DisplayProps();
+            Console.WriteLine("======");
+            Chair c2 = new Chair("orange", 50, false);
+            c2.DisplayProps();// invocation
         }
 
     }
